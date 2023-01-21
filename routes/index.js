2cc048453a61dedacf3f8dafeaf1e61e83eec841
BellaBradbury <bellabradbury@yahoo.com>
2023-01-21 01:35:37 -0500

    basic views created and written

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+// sequelize
+var pug = require('pug');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// async handler
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(err){
+      res.status(500).send(err);
+    }
+  }
+}
+
+// GET home page
+router.get('/', asyncHandler( async(req, res) => {
+  res.redirect('/home');
+}));
+router.get( '/home', asyncHandler (async(req, res) => {
+  res.render('index');
+}));
+
+// GET story page
+router.get( '/story', asyncHandler (async(req, res) => {
+  res.render('story');
+}));
+
+// GET news page
+router.get( '/news', asyncHandler (async(req, res) => {
+  res.render('news');
+}));
+
+// GET gallery page
+router.get( '/gallery', asyncHandler (async(req, res) => {
+  res.render('gallery');
+}));
 
 module.exports = router;
